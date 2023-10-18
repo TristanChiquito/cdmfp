d41739b9b777f3917f5045d0929936aca666ff7c
Tristan Kempes 2005 <tristankempes@gmail.com>
2023-10-18 13:58:00 +0200

    app.css

---
+++ Asignaturas/Deberes/Big_data/Rstudio/Ejercicio01.R
@@ -0,0 +1,16 @@
+getwd
+setwd
+data
+data()
+data(package = .packages(all.available = TRUE))
+mtcars
+names(x)
+mtcars
+names(mtcars)
+names
+mtcars$wt
+v <- c("a,b,c,d,e,f,g,h")
+x <- c("i,j,k,l,m,n,o,p")
+z <- c("q,r,s,t,u,v,w,x")
+matriz <- rbind(v,x,z)
+v[1,5]
---
+++ Asignaturas/Deberes/Big_data/Rstudio/Prueba.R
@@ -0,0 +1,32 @@
+x <- 5
+class(x)
+x <- 5L
+x <- "production"
+x <- TRUE
+x <- as.character(x)
+x <- "1"
+x <- as.logical(x)
+v <- c(2,3,4,5)
+length(v)
+length <- x("shi")
+class(v)
+horas.extra<-c(1.5,1,0,1,0)
+names(horas.extra)<-c("Lunes","Martes","miercoles","Jueves","Viernes")
+horas.extra[5]
+v[c(1,2,3,5)]
+horas.extra[-4]
+horas.extra[c(1,2,3)]
+horas.extra[1:3]
+horas.extra[-(4:5)]
+horas.extra[c(2,3,5)]
+indice<-c(2,3,5)
+horas.extra[-indice]
+w<-c("5","pepe","56","uhilu")
+rbind(v,w)
+cbind(v,w)
+2 <- x("pacoSanz","gaga","agugu","siu")
+w <- c(5,6,7,8)
+x <- c(9,10,11,12)
+m <- rbind(v,w,x)
+
+
